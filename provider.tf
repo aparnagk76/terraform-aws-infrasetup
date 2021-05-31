e7b6d8afc1e1079fae5612547cc20c407d5431ba
aparnagk76 <aparnagk76@gmail.com>
2021-05-31 15:54:40 -0700

    add remote s3 backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend s3 {
+    bucket = "aparna-terraform-state"
+    key = "infra-setup/state"
+    region = "us-west-1"
+  }
 }
 
 provider "aws" {
